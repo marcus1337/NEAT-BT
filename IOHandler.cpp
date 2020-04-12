12faed1c7e99cf07a527eec96131979c18a3e56c
marcus1337 <blurgas@hotmail.com>
2020-04-12 14:34:56 +0200

    IOHandler

--- IOHandler.cpp
+++ IOHandler.cpp
@@ -1,10 +1,63 @@
 #include "IOHandler.h"
 #include <filesystem>
 #include <iostream>
+#include <stack>
+
 namespace fs = std::filesystem;
 
+std::string IOHandler::getPath(std::string fileName) {
+    std::string filePath = std::filesystem::current_path().string();
+    filePath += "\\" + fileName;
+    return filePath;
+}
+
 void IOHandler::makeFolder(std::string folderName) {
-    std::string folderPath = std::filesystem::current_path().string();
-    folderPath += "\\" + folderName;
-    fs::create_directory(folderPath);
+    fs::create_directory(getPath(folderName));
+}
+
+std::string IOHandler::getFolderName(int generation, std::string folderName) {
+    return std::string(folderName + "_" + std::to_string(generation));
+}
+
+std::string IOHandler::getFilenameWithPath(std::string folderNameAndGeneration, int treeIndex) {
+    return getPath(std::string(folderNameAndGeneration + "//TREE_" + std::to_string(treeIndex)+ ".txt"));
+}
+
+std::ofstream IOHandler::getFileStream(int treeIndex, int generation, std::string folderName) {
+    std::string folderNameAndGeneration = getFolderName(generation, folderName);
+    makeFolder(folderNameAndGeneration);
+    return std::ofstream(getFilenameWithPath(folderNameAndGeneration, treeIndex));
+}
+
+std::string IOHandler::getParentNodeString(Node* node) {
+    std::string res;
+    res += parentStartTag + std::string(" " + std::to_string(node->type) + " " + std::to_string(node->ID)) + '\n';
+    for (auto& n : node->children)
+        res += std::string(std::to_string(n.type) + " " + std::to_string(n.ID)) + '\n';
+    res += parentEndTag + '\n';
+    return res;
+}
+
+void IOHandler::saveTree(Tree& tree, int treeIndex, int generation, std::string folderName) {
+    std::ofstream stream = getFileStream(treeIndex, generation, folderName);
+
+    std::vector<Node*> interiorNodes = extractInteriorNodes(tree);
+    for (auto& node : interiorNodes) {
+        stream << getParentNodeString(node) << '\n';
+    }
+}
+
+std::vector<Node*> IOHandler::extractInteriorNodes(Tree& tree) {
+    std::vector<Node*> interiors;
+    std::stack<Node*> nodes;
+    nodes.push(&tree.root);
+    while (!nodes.empty()) {
+        Node* tmp = nodes.top();
+        nodes.pop();
+        for (auto& n : (*tmp).children)
+            if (n.isParent())
+                nodes.push(&n);
+        interiors.push_back(tmp);
+    }
+    return interiors;
 }
--- IOHandler.h
+++ IOHandler.h
@@ -1,15 +1,31 @@
 #include "Tree.h"
 #include <vector>
 #include <string>
+#include <fstream>
+
+#ifndef IOHANDLER_H
+#define IOHANDLER_H
+
 
 class IOHandler {
-public:
+private:
     void makeFolder(std::string folderName);
+    std::vector<Node*> extractInteriorNodes(Tree& tree);
+    std::string getPath(std::string folderName);
+    std::string getFolderName(int generation, std::string folderName);
+    std::string getFilenameWithPath(std::string folderNameAndGeneration, int treeIndex);
+    std::ofstream getFileStream(int treeIndex, int generation, std::string folderName);
+    std::string getParentNodeString(Node* node);
+
+    std::string parentStartTag = "<P>";
+    std::string parentEndTag = "</P>";
 
 public:
     std::vector<Tree> loadGeneration();
-    Tree loadTree();
-    void saveTree(Tree& tree, int generation, std::string folderName = "TREES");
-    void saveGeneration(std::vector<Tree>& trees, int generation, std::string folderName = "TREES");
+    //Tree loadTree();
+    void saveTree(Tree& tree, int treeIndex, int generation, std::string folderName = "TREES");
+    //void saveGeneration(std::vector<Tree>& trees, int generation, std::string folderName = "TREES");
 
 };
+
+#endif
