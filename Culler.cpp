f6ddc73832e009edbc91f88aed18c36954da4024
marcus1337 <blurgas@hotmail.com>
2020-05-01 17:07:12 +0200

    remove old culling

--- Culler.cpp
+++ Culler.cpp
@@ -67,36 +67,6 @@ std::vector<float> Culler::getSpecieProbabilities(std::vector<Specie>& oldSpecie
     return specieProbabilities;
 }
 
-/*bool Culler::isWeak(const Specie& specie, int numSpecies, int totalAverageFitness) {
-    return specie.getSpecieStrength(numSpecies, totalAverageFitness) < 0.8f;
-}*/
-
-/*void Culler::removeWeakSpecies(std::vector<Specie>& species) {
-    int numSpecies = species.size();
-    int totalAverageFitness = getTotalAverageFitness(species);
-    species.erase(std::remove_if(species.begin(), species.end(),
-        [&](const Specie& o) { return isWeak(o, numSpecies, totalAverageFitness); }), species.end());
-}
-void Culler::removeWeaksInSpecies(std::vector<Specie>& species) {
-    for (Specie& spec : species) {
-        std::vector<Tree*> survivors;
-        int remaining = spec.trees.size() / 2;
-        if (remaining == 0)
-            survivors.push_back(spec.trees[0]);
-        for (int i = 0; i < remaining; i++)
-            survivors.push_back(spec.trees[i]);
-        spec.trees = survivors;
-    }
-    species.erase(std::remove_if(species.begin(), species.end(),
-        [](const Specie& o) { return o.trees.size() == 0; }), species.end());
-}*/
-
-/*void Culler::cullAllButOneFromSpecies(std::vector<Specie>& species) {
-    for (Specie& spec : species) {
-        spec.trees = std::vector<Tree*>({ spec.trees[0] });
-    }
-}*/
-
 int Culler::getTotalAverageFitness(std::vector<Specie>& species) {
     int total = 0;
     for (const auto& s : species)
